574acfa1db5b3d6b4a0ec043c7e5199b55318030
peyman-moshfegh <moshfegh_peyman@yahoo.com>
2021-10-28 12:40:52 -0400

    third commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/contact_list"
+    //"URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://PeterM:ZFlONmNLgPv7knEb@mongodbserver.yil0d.mongodb.net/contacts?retryWrites=true&w=majority"
 }
